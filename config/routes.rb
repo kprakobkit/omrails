658e74a6fd17c49c762630226c36cd75255610f4
Peter Prakobkit <kprakobkit@gmail.com>
2013-04-27 00:49:36 -0700

    Added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Omrails::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +51,6 @@ Omrails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
